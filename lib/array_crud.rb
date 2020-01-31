970cd9526605f5eb0b3b2982f2cc423b2befd331
excellent-validator-4322 <plarmony24@gmail.com>
2020-01-31 05:28:36 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["yellow","blue","red","green"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array,element)
+  array.push (element)
 end
 
 def add_element_to_start_of_array(array, element)
   
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_element_from_end_of_array=array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array=array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(array,index_number )
+  retrieve_element_from_index=array [index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  retrieve_first_element_from_array=array [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array=array[-1]
 end
