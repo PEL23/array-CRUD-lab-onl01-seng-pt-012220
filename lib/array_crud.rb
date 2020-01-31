02622ddfce91527a4d0ccd6f4f472f8a9dad912b
excellent-validator-4322 <plarmony24@gmail.com>
2020-01-31 05:25:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,11 +20,11 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift(element)
+  remove_element_from_start_of_array=array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = [1]
+  array = []
 end
 
 def retrieve_first_element_from_array(array)
